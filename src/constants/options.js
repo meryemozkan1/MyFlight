d8d238b6bbc105691b716bf81b9ee31efedd209c
meryemozkan1 <meryemozkan1@users.noreply.github.com>
2024-09-24 23:03:40 +0300

    first commit

---
+++ src/constants/options.js
@@ -0,0 +1,13 @@
+export const time = [
+  { content: "5:00 AM - 11:59 AM" },
+  { content: "12:00 PM - 5:59 PM" },
+];
+
+export const stops = [
+  { content: "Nonstop" },
+  { content: "1 Stop" },
+  { content: "2+ Stops" },
+];
+
+
+// mock data for sorting card
